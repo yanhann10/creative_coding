49ea3b38ff2f4220b5f485ce1fc00f013b81a60c
yanhann10 <yanhann10@gmail.com>
2019-01-28 20:13:05 -0500

    add color gradient

--- p5/sketches/topography/cross_section.js
+++ p5/sketches/topography/cross_section.js
@@ -4,13 +4,19 @@ let cross=[];
 let cross_num=1;
 let w=800, h=600;
 
+
 function setup() {
   createCanvas(w, h);
+  noiseSeed(99);
   background(255);
   angleMode(DEGREES);
+  colorMode(RGB);
+  //start n stop of color range
+  let c1 = color(7,15,81); //blue
+  let c2 = color(255); //white
   //for (let i=0; i<cross_num; i++){
                             // x           , y             ,rad, num
-    sec = new CrossSec(300, 300, 80, 5);
+    sec = new CrossSec(300, 300, 80, 5,c1,c2);
  // }
 }
 
@@ -24,17 +30,21 @@ function draw() {
 
 class CrossSec{
     //isolines
-    constructor(tempX, tempY, tempR,tempN) {
+    constructor(tempX, tempY, tempR,tempN,tempC1, tempC2) {
         this.x=tempX;
         this.y=tempY;
         this.r=tempR;
-        this.n=tempN
+        this.n=tempN;
+        this.c1=tempC1;
+        this.c2=tempC2;
     }
     
     display() {
         for (let j=0;j<this.n; j++){
+            let interC = lerpColor(this.c1,this.c2, j/(this.n));
             beginShape();
             push()
+            fill(interC);
             translate(this.x, this.y);
             for (let i=0;i<90;i++) {
                 let radius=this.r+ map(noise(i/5),0,1,6,60) - 20 * j;
