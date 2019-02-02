f695716e4ada55cfb241b8080386e8e7556526bd
yanhann10 <yanhann10@gmail.com>
2019-02-02 13:55:53 -0500

    add crystal proj

--- p5/sketches/topography/crystal/crystal.js
+++ p5/sketches/topography/crystal/crystal.js
@@ -1,38 +1,35 @@
 //shapes that resemble the cross-section of cut crystal
 let fill_color =[];
-let cross=[];
-let cross_num=5;
 let w=1200, h=500;
 let frameRt=3;
 
 function setup() {
   frameRate(frameRt);
-  createCanvas(w, h);
+  createCanvas(windowWidth, windowHeight);
   noiseSeed(99);
   background(255);
   angleMode(DEGREES);
   colorMode(RGB);
   //start n stop of color range
-  let c1 = [color(167,34,110), color(236,32,73), color(242,107,56), 
-    color(247,219,79), color(47,149,153)]; //purple
+  let c1 = color(167,34,110); //purple
   let c2 = color(255); //white
-  for (let i=0; i<cross_num; i++){
-    cross[i] = new CrossSec(230*i+150, //x
-        250, //y
-        80, //radius
-        random(3,7), //num of rings
-        c1[i], //dark color
+
+    cross = new CrossSec(windowWidth-20, //x
+        windowHeight-20, //y
+        800, //radius
+        18, //num of rings
+        c1, //dark color
         c2);
   }
 
-}
+
 
 function draw() {  
 
-    background(0);
-    for (let i=0; i<cross_num; i++){
-        cross[i].display(); 
-    }
+    background(options.Background);
+
+        cross.display(); 
+
 }
 
 
@@ -58,17 +55,20 @@ class CrossSec{
             fill(interC);
             translate(this.x, this.y);
             for (let i=0;i<90;i++) {
-                let radius= map(noise(mynoiseseed+this.r), 0, 1, 18, 128) + 
-                    map((noise(mynoiseseed+i/5)),0,1,10,60) - dist * j;
-                let x=sin(4*i);
+                let radius= this.r + 
+                //this.r doesn't work here
+                    map((noise(mynoiseseed+i/5)),0,1,10,120) - dist * j;
+                let x=1.5*sin(4*i);
                 let y=cos(4*i);
                 vertex(radius*x, radius*y);
             }
             endShape(CLOSE);
             pop();
-           
+            noLoop();
             }
         }
-}
-//to-consider: how to make noise more concentrated in certain range..log?
-//change speed
+      }
+
+//to-do: make the heart of crystal appear randomly at either upper right or lower right
+//make radius vary
+//adjust color
