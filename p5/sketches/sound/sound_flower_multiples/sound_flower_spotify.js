821f55823715de1024eedce317243e7090e99515
yanhann10 <yanhann10@gmail.com>
2019-02-10 09:36:38 -0500

    updated shape

--- p5/sketches/sound/sound_flower_multiples/sound_flower_spotify.js
+++ p5/sketches/sound/sound_flower_multiples/sound_flower_spotify.js
@@ -1,15 +1,27 @@
 // An Array of Bubble objects
 //shapes that resemble the cross-section of cut crystal
 
+/*
+ENCODINGS
+loudness->size
+low valence->sad sounds->purple
+high valence->happy songs->green
+label->name
+*/
+
+
 let fill_color = [];
-let w = 1200,
-  h = 500;
+let w = 1200;
+let h = 500;
 let frameRt = 3;
-let n_flo = 20
+let col =['#317050','#20726A','#31717F','#566C89','#7B6487','#985C78']
+//let col = ["#5901a5", "#1db954"]
 
 var bubbles;
-// A Table object
+// A Table object for data
 var table;
+// A slider for testing
+let Slider;
 
 function preload() {
   table = loadTable("data/SpotifySample.csv", "header");
@@ -18,11 +30,15 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   loadData();
+  //create slider
+  Slider = createSlider(10, 50, 10);
+  Slider.position(50, 50);
 }
 
 function draw() {
   background(255);
-  // Display all bubbles
+  let p = Slider.value();
+
   for (var i = 0; i < bubbles.length; i++) {
     bubbles[i].display();
     bubbles[i].rollover(mouseX, mouseY);
@@ -31,88 +47,90 @@ function draw() {
 }
 
 function loadData() {
-  // Load CSV file into a Table object
-  // "header" option indicates the file has a header row
-
-  // The size of the array of Bubble objects is determined by the total number of rows in the CSV
   bubbles = [];
-  flos = [];
-  // You can access iterate over all the rows in a table
+
   for (var i = 0; i < table.getRowCount(); i++) {
     var row = table.getRow(i);
-    // You can access the fields via their column name (or index)
     var r = row.get("loudness");
     var n = row.get("energy");
+    var v = row.get("valence");
     var txt = row.get('track_name');
     // Make a Bubble object out of the data read
     bubbles[i] = new CrossSec(random(80, 1200), //x
       random(80, 800), //y
       8 * r, //radius
-      floor(n * 9),
-      color("#5901a5"), //purple
-      color(73, 59, 149), //navy
+      floor(n * 6),
+      v,
       txt
     );
-
-    //new Bubble(random(0,500), random(0,500), r, txt);
 
   }
 }
 
 
 class CrossSec {
+
   //isolines
-  constructor(tempX, tempY, tempR, tempN, tempC1, tempC2, tempName) {
+  constructor(tempX, tempY, tempR, tempN, tempC, tempName) {
     this.x = tempX;
     this.y = tempY;
     this.r = -Number(tempR);
     this.n = tempN;
-    this.c1 = tempC1;
-    this.c2 = tempC2;
+    this.c = tempC;
     this.name = tempName;
     this.over = false;
   }
 
   // Checking if mouse is over the Bubble
   rollover(px, py) {
-    var d = dist(px, py, this.x, this.y);
-    //this.x, this.y);
+    let d = dist(px, py, this.x, this.y);
     this.over = (d < 10000);
-    //this.radius);
-    //console.log(this.over);
   }
 
   display() {
+
     let mynoiseseed = random(1000);
     let dist = floor(this.r / this.n)
     for (let j = 0; j < this.n; j++) {
-      //repeat light to dark color gradation
-      let interC = lerpColor(this.c1, this.c2, j % (this.n / 2 + 1) / (this.n / 2 + 1));
-      stroke(interC);
+      //lerp could generate some serious ugly col
+      //let interC = lerpColor(color("#5901a5"), color("#1db954"), j % (this.n / 2 + 1) / (this.n / 2 + 1));
+      // if (this.c <0.5) {
+      //   stroke(color("#5901a5"));}
+      //   else if (this.c>0.5 & this.c<0.7) {
+      //     stroke(126,175,228);}       
+      // else {stroke(color("#00a86b"));}
+      stroke(color("#5901a5"));
       strokeWeight(0.4);
       beginShape();
       push()
       //fill(interC);
       translate(this.x, this.y);
+
       for (let i = 0; i < 100; i++) {
         let radius = this.r +
           map((noise(mynoiseseed)), //increase this for rounder shape
             0, 1, 10, 80) -
           dist * j;
-        let x = sin(4 * i);
-        let y = cos(4 * i); //3: sea urchin
+        let p=4.2;
+        let x = sin(p * i);
+        let y = cos(p * i);
+        //3: sea urchin
+        //4.6: geometric
+        //4.2 cool shape //8.8 ->5 petal
+        //8.6 refractory rays
+        //12.6 half pie
         vertex(radius * x, radius * y);
       }
-      endShape(CLOSE);
 
+
+      endShape(CLOSE);
 
       //if (this.over) {
       textAlign(CENTER);
       noStroke();
       fill(0);
       pop();
-      text(this.name, this.x - this.r / 2, this.y);
-
+      text((this.name), this.x - this.r / 2, this.y);
       //}
 
       noLoop();
@@ -122,8 +140,9 @@ class CrossSec {
 }
 
 //to-do: 
-//text label
-//find better color
-//softer edges
-//more flowers
+//get slider to work
+//text labels upon mouseover
+//find better palette 
+//more data
+//vary #petal 
 //text2vec layout
