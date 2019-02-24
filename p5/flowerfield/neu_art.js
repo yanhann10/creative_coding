d9aca08304f9c208b925e5a827474e1bc8ac63b2
yanhann10 <yanhann10@gmail.com>
2019-02-23 20:55:48 -0500

    modularized

--- p5/flowerfield/neu_art.js
+++ p5/flowerfield/neu_art.js
@@ -1,41 +1,68 @@
 function setup() {
-    createCanvas(800, 700);
+    createCanvas(1000, 700);
     // background(48);
-    petals();
-    stalk();
-  }
-  
-  function petals() {
-    // noStroke();
-  
+    dand = new Dandelion(300, 0, 50,100);
+}
 
-    for (var i = 0; i < 100; i++) {
-      var r = random(600);
-      // fill(color(random(200, 255), random(200, 255), random(255), 78)) // yellow
-      fill(color(random(255), random(200, 210), random(200, 255), 78)) // blue
-  
-      beginShape();
-    //   curveVertex(400, 0);
-    //   curveVertex(400, 350);
-    //   curveVertex(random(400), random(600));
-    //   curveVertex(random(400), random(400));
-      curveVertex(200, 0);
-      curveVertex(200, 350);
-      curveVertex(random(400), random(260));
-      curveVertex(random(400), random(200));
-      endShape();
+function draw() {
+
+    background(255);
+    dand.display();
+
+}
+
+class Dandelion {
+    constructor(tempX, tempY, tempN, tempL, tempS) {
+        this.x = tempX; 
+        this.y = tempY;
+        this.n = tempN; //number of petals
+        this.l = tempL; //length
+        this.s = tempS; //span: wide or narrow flowers
     }
+
+
+
+display() {
+
+    for (var i = 0; i < this.n; i++) {
+        //set starting position
+        
+        
+        var r = random(600);
+        // fill(color(random(200, 255), random(200, 255), random(255), 78)) // yellow
+        fill(color(random(255), random(200, 210), random(200, 255), 118)) // blue
+
+        beginShape();
+        let l=350;
+        push();
+        stroke (10,100);
+        translate(this.x, this.y);
+        //   curveVertex(400, 0);
+        //   curveVertex(400, 350);
+        //   curveVertex(random(400), random(600));
+        //   curveVertex(random(400), random(400));
+        
+        curveVertex(0, 0);
+        curveVertex(0, this.l);
+        curveVertex(random(-0.9*this.l, 0.9*this.l), random(0.8*this.l));
+        curveVertex(random(-0.9*this.l, 0.9*this.l), random(1.2*this.l));
+        endShape();
+        pop();
+    }
+    for (var i = 0; i < 10; i++) {
+        push();
+        translate(this.x, this.y);
+        fill(color(144, random(200, 255), 144, 60));
+        beginShape();
+        curveVertex(0, i);
+        curveVertex(0, this.l);
+        curveVertex(0, 1.6*this.l);
+        curveVertex(random(0.5*this.l), random(0.5*this.l));
+        endShape();
+        pop();
+    }
+
    
-  }
-  
-  function stalk() {
-    for (var i = 0; i < 30; i++) {
-      fill(color(144, random(200, 255), 144, 60));
-      beginShape();
-      curveVertex(200, i);
-      curveVertex(200, 350);
-      curveVertex(200, 600);
-      curveVertex(random(400), random(600));
-      endShape();
-    }
-  }
+noLoop();
+}
+}
