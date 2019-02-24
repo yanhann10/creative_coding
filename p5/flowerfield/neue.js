268527e1418ce09cb2678fa23b67720f7dd9e8ba
yanhann10 <yanhann10@gmail.com>
2019-02-23 21:30:43 -0500

    update param

--- p5/flowerfield/neue.js
+++ p5/flowerfield/neue.js
@@ -3,36 +3,38 @@ let n_flo=600;
 let buffer = 20; //avoid shape cut-off at the edge of the screen
 
 function setup() {
+    angleMode(DEGREES);
     createCanvas(displayWidth, displayHeight);
     // background(48);
     for (let i = 0; i < n_flo; i++) {
         //dand = new Dandelion(300, 0, 50,100);
         flowerfield[i] = new Dandelion(random(buffer, displayWidth - buffer),
             random(buffer, displayHeight - buffer),
-            random(20, 80),
-            random(5, 60));
+            random(20, 80),//number of petals
+            random(5, 60),//length
+            random(0.2,1),//span: wide or narrow flowers range 0.5 to 1.2
+            0);//rotation angle
     }
 
 }
 
 function draw() {
-
+    
     background(255);
-    //dand.display();
     for (let i = 0; i < n_flo; i++) {
-        //dand = new Dandelion(300, 0, 50,100);
         flowerfield[i].display();
     }
 
 }
 
 class Dandelion {
-    constructor(tempX, tempY, tempN, tempL, tempS) {
+    constructor(tempX, tempY, tempN, tempL, tempW, tempRotate) {
         this.x = tempX;
         this.y = tempY;
         this.n = tempN; //number of petals
         this.l = tempL; //length
-        this.s = tempS; //span: wide or narrow flowers
+        this.w = tempW; //width flowers range 0.5 to 1.2
+        this.rotate=tempRotate; //rotation angle
     }
 
 
@@ -40,26 +42,19 @@ class Dandelion {
     display() {
 
         for (var i = 0; i < this.n; i++) {
-            //set starting position
-
-
             var r = random(600);
-            // fill(color(random(200, 255), random(200, 255), random(255), 78)) // yellow
-            fill(color(random(255), random(200, 210), random(200, 255), 118)) // blue
-
+            fill(color(random(255), random(150, 210), random(200, 255), 118)) // blue
+            //set starting position
             beginShape();
             let l = 350;
             push();
-            stroke(10, 100);
-            translate(this.x, this.y);
-            //   curveVertex(400, 0);
-            //   curveVertex(400, 350);
-            //   curveVertex(random(400), random(600));
-            //   curveVertex(random(400), random(400));
-
+            
+            translate(this.x, this.y); //translate b4 rotate
+            rotate(this.rotate);
+            stroke(10, 60);
             curveVertex(0, 0);
             curveVertex(0, this.l);
-            curveVertex(random(-0.9 * this.l, 0.9 * this.l), random(0.8 * this.l));
+            curveVertex(random(-this.w * this.l, this.w * this.l), random(0.8 * this.l));
             curveVertex(random(-0.9 * this.l, 0.9 * this.l), random(1.2 * this.l));
             endShape();
             pop();
@@ -67,6 +62,7 @@ class Dandelion {
         for (var i = 0; i < 10; i++) {
             push();
             translate(this.x, this.y);
+            rotate(this.rotate);
             fill(color(144, random(200, 255), 144, 60));
             beginShape();
             curveVertex(0, i);
