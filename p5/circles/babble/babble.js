6238de22ea8494933cda46d4a8ae23313eac3f5d
yanhann10 <yanhann10@gmail.com>
2019-03-03 00:51:12 -0500

    consolidated 3 formats

--- p5/circles/babble/babble.js
+++ p5/circles/babble/babble.js
@@ -1,51 +1,60 @@
 //tangent circles of different radius
 function setup() {
-    createCanvas(600, 600);
-    myBabble = new Babble(300, 300, 5, 5);
+    frameRate(1);
+    createCanvas(windowWidth, windowHeight);
+    myBabble = new Babble(windowWidth/2, windowHeight/3, 20, 
+        60, 3, 2, 1);
 }
 
 
 function draw() {
     background(255);
-    myBabble.display_Random();
+    myBabble.display();
 }
 
 class Babble {
-    constructor(tempX, tempY, tempN, tempD, tempAdj = 0) {
-        this.x = tempX;
+    constructor(tempX, tempY, tempS,
+        tempN, tempD, tempAdj = 0, tempRandom) {
+        this.x = tempX; //starting position
         this.y = tempY;
+        this.s = tempS; //starting size
         this.n = tempN; //number of circles
         this.d = tempD; //distance in between 
         this.adj = tempAdj; //adjustments to make the circles not touching
-
+        this.rand = tempRandom;
     }
 
     display() {
+        console.log(this.rand);
+        if (this.rand==1) {
+            //random format, circles are tangent to each other at random points
+            let r = this.d;
+            translate(this.x, this.y);
+            noFill();
+            for (let i = 0; i < this.n; i++) {
+                let randomNoise = random(0, 180);
+                let xr = cos(radians(randomNoise)),
+                    yr = sin(radians(randomNoise));
+                translate(r * xr, r * yr);
+                ellipse(0, 0, this.s + 2 * i * r, this.s + 2 * i * r);              
+            }
+            noLoop()
+
+
+        }
+        else {
+            //uniform format, tangent point overlapping (this.adj=0)
+            //or on the same line (this.adj!=0)
         translate(this.x, this.y);
         noFill();
         for (let i = 0; i < 5; i++)
             ellipse(0, i * (this.d - this.adj),
-                60 + 2 * this.d * i, 60 + 2 * this.d * i);
+                this.s + 2 * this.d * i, this.s + 2 * this.d * i);
+            }
     }
 
 
-    display_Random() {
-        let r = this.d;
-        translate(this.x, this.y);
-        noFill();
-        point(0, 0);
-
-        for (let i = 0; i < this.n; i++) {         
-            let randomNoise = random(0, 180);
-            let xr = cos(radians(randomNoise)),
-                yr = sin(radians(randomNoise));
-            translate(r * xr, r * yr);
-            ellipse(0, 0, 60 + 2 * i * r, 60 + 2 * i *r);
-
-            
-        }
-        noLoop()
-    }
+  
 
 }
 
