bf718ec3e5844319bf1cb2ac51046296dae92156
yanhann10 <yanhann10@gmail.com>
2019-03-08 00:08:06 -0500

    small fix

--- p5/geometry/hexagon.js
+++ p5/geometry/hexagon.js
@@ -1,36 +1,35 @@
 let n = 6;
-let r;
+let r = 60;
 let angle = 360 / n;
 
 function setup() {
     createCanvas(600, 600);
     colorMode(HSB, 360, 100, 100);
+    frameRate(10);
 }
 
 function draw() {
     background(255);
-
-
-
-    for (let x = 0; x<3; x++) { 
-        hexagon(100+x*300, 100);
-        hexagon(250+x*300, 100 + 100 * sin(radians(60)));
+    randomSeed(10);
+    let nX = 300 / r / 2;
+    let nY = 600 / r / 2;
+    for (let x = 0; x < nX; x++) {
+        for (let y = 0; y < nY; y++) {
+            let h = r * sin(radians(60));
+            hexagon(r + x * r * 3, r + 2 * h * y);
+            hexagon(150 + x * r * 3, r + h + 2 * h * y);
+        }
     }
-
-
-   
 }
 
 function hexagon(x, y) {
     beginShape(TRIANGLE_FAN);
     vertex(x, y);
-    let r = 100;
     for (let i = 0; i <= n; i++) {
         let pX = x + r * cos(radians(angle) * i)
         let pY = y + r * sin(radians(angle) * i)
         vertex(pX, pY);
         fill(angle * i + 80, 50, 100);
-
     }
     endShape();
 }
