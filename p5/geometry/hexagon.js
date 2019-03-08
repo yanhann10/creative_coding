fe3c2f0edca32b8bea05596d0193a3c0f44e0e52
yanhann10 <yanhann10@gmail.com>
2019-03-08 00:12:08 -0500

    add mouse move

--- p5/geometry/hexagon.js
+++ p5/geometry/hexagon.js
@@ -13,11 +13,12 @@ function draw() {
     randomSeed(10);
     let nX = 300 / r / 2;
     let nY = 600 / r / 2;
+    let shiftX = map(mouseX, 0, 600, 1.8*r, 2.5*r)
     for (let x = 0; x < nX; x++) {
         for (let y = 0; y < nY; y++) {
             let h = r * sin(radians(60));
             hexagon(r + x * r * 3, r + 2 * h * y);
-            hexagon(150 + x * r * 3, r + h + 2 * h * y);
+            hexagon(shiftX + x * r * 3, r + h + 2 * h * y);
         }
     }
 }
@@ -33,3 +34,4 @@ function hexagon(x, y) {
     }
     endShape();
 }
+
