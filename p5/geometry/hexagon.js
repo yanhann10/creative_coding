8b37fcb57da193861f74e55d6dbc534891029a6e
yanhann10 <yanhann10@gmail.com>
2019-03-07 23:45:24 -0500

    initial

---
+++ p5/geometry/hexagon.js
@@ -0,0 +1,36 @@
+let n = 6;
+let r;
+let angle = 360 / n;
+
+function setup() {
+    createCanvas(600, 600);
+    colorMode(HSB, 360, 100, 100);
+}
+
+function draw() {
+    background(255);
+
+    beginShape(TRIANGLE_FAN);
+
+    for (let x = 0; x<3; x++) { 
+        hexagon(100+x*300, 100);
+        hexagon(250+x*300, 180);
+    }
+    
+
+   
+}
+
+function hexagon(x, y) {
+    beginShape(TRIANGLE_FAN);
+    vertex(x, y);
+    let r = 100;
+    for (let i = 0; i <= n; i++) {
+        let pX = x + r * cos(radians(angle) * i)
+        let pY = y + r * sin(radians(angle) * i)
+        vertex(pX, pY);
+        fill(angle * i + 80, 50, 100);
+
+    }
+    endShape();
+}
