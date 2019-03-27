91b0a6e9f7dca29e33df69a13875f2efefa5495d
yanhann10 <yanhann10@gmail.com>
2019-03-27 00:32:57 -0400

    add draft

--- p5/mappa/map_flow/scratch.js
+++ p5/mappa/map_flow/scratch.js
@@ -1,95 +1,125 @@
-
 //based on https://github.com/cvalenzuela/Mappa
 
-var canvas;
-var myMap;
-var tripsCoordinates;
-var allCoordinates = [];
-var data;
+let canvas;
+let myMap;
+let tripsCoordinates;
+let allCoordinates = [];
+let data;
 
-var delta = 0; 
-var coordinate = 0; 
+let delta = 0;
+let coordinate = 0;
 
-var origin; 
-var originVector;  
-var destination; 
-var destinationVector;
+let origin;
+let originVector;
+let destination;
+let destinationVector;
 
-var taxiPosition;
+let taxiPosition;
 
-var visitedRoutes = []; // A new array to hold all visited positions
-var key = 'AIzaSyCdmGJ7JWNWkmmxSdvNiu1Qo6BMn56eTSE';
+let visitedRoutes = []; // A new array to hold all visited positions
+let key = 'AIzaSyCdmGJ7JWNWkmmxSdvNiu1Qo6BMn56eTSE';
 
-var options = {
-  lat: 40.73447,
-  lng: -74.00232,
-  zoom: 4,
-  styles: style
+let options = {
+    lat: 40.73447,
+    lng: -74.00232,
+    zoom: 4,
+    styles: style
 }
-var mappa = new Mappa('Google', key);
+let mappa = new Mappa('Google', key);
+
+let myPath;
 
 function preload() {
-  data = loadJSON('./data/taxiday1.geojson');
+    data = loadJSON('./data/taxiday1.geojson');
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  myMap = mappa.tileMap(options);
-  myMap.overlay(canvas); 
-  tripsCoordinates = myMap.geoJSON(data, "LineString");
+    canvas = createCanvas(windowWidth, windowHeight);
+    myMap = mappa.tileMap(options);
+    myMap.overlay(canvas);
+    tripsCoordinates = myMap.geoJSON(data, "LineString");
 
-  tripsCoordinates.forEach(function (trip) {
-    trip.forEach(function (coordinate) {
-        allCoordinates.push(coordinate)
-      })
-  });
-  //myMap.onChange(drawPoints);
-  myMap.onChange(drawRoute); // Everytime the map is zoomed or moved update the route
+    tripsCoordinates.forEach(function (trip) {
+        trip.forEach(function (coordinate) {
+            allCoordinates.push(coordinate)
+        })
+    });
+    let o =(40, -74);
+    let d = (17, -100);
+    
+    myPath = new Path(o,d);
 }
 
-function draw(){
-  //clear(); clear can be commented since drawRoute() will handle clearing the canvas
-  if(delta < 1){
-    delta += 0.2; 
-  } else {
-    visitedRoutes.push(allCoordinates[coordinate]) // Once it has arrived at its destination, add the origin as a visited location
-    delta = 0; 
-    coordinate ++;
-    drawRoute(); // Call the drawRoute to update the route
-  }
+function draw() {
+    //clear(); clear can be commented since drawRoute() will handle clearing the canvas
+    if (delta < 1) {
+        delta += 0.2;
+    } else {
+        visitedRoutes.push(allCoordinates[coordinate]) // Once it has arrived at its destination, add the origin as a visited location
+        delta = 0;
+        coordinate++;
+        //drawRoute(); // Call the drawRoute to update the route
+    }
 
-  
+    myPath.display();
 
-  noStroke(); // remove the stroke from the route
-  fill(255,255,0);
- 
+    noStroke(); // remove the stroke from the route
+    fill(255, 255, 0);
 
-  o2=myMap.latLngToPixel(40, -74);
-  d2=myMap.latLngToPixel(37,-122);
-  o2Vector = createVector(o2.x, o2.y); 
-  d2Vector = createVector(d2.x, d2.y); 
-  for (i=0;i<=1000;i++) {
 
-    pos = o2Vector.lerp(d2Vector, frameCount/1000); 
-    ellipse(pos.x, pos.y, 10, 10);
-    
-  }
+    // o = myMap.latLngToPixel(40, -74);
+    // d1 = myMap.latLngToPixel(17, -100);
+    // d2 = myMap.latLngToPixel(37, -122);
+    // oVector = createVector(o.x, o.y);
+    // console.log(oVector);
+    // d1Vector = createVector(d1.x, d1.y);
+    // d2Vector = createVector(d2.x, d2.y);
+    // for (i = 0; i <= 1000; i++) {
+    //     if (frameCount<=1000){
+    //         pos = oVector.lerp(d1Vector, frameCount / 1000);
+    //         //pos2 = createVector(o.x, o.y).lerp(d2Vector, frameCount / 1000);
+    //         ellipse(pos.x, pos.y, 10, 10);
+    //         //ellipse(pos2.x, pos2.y, 10, 10);
+    //     }
+    // }
 }
 
 
 
 //This functions draws a line with n-vertices where n = visited routes;
-function drawRoute(){
-  clear();
-  stroke(255,0,0, 40); // stroke color and width to see the route line
-  strokeWeight(5);
-  //if(visitedRoutes.length > 0){
-    noFill();
-    beginShape();
-    visitedRoutes.forEach(function (e) {
-        //var pos = myMap.latLngToPixel(e[1], e[0]);
-        vertex(pos.x, pos.y);
-    })
-    endShape()
-  //}
+// function drawRoute() {
+//     clear();
+//     stroke(255, 0, 0, 40); // stroke color and width to see the route line
+//     strokeWeight(5);
+//     noFill();
+//     beginShape();
+//     visitedRoutes.forEach(function (e) {
+//         vertex(pos.x, pos.y);
+//     })
+//     endShape()
+// }
+
+class Path {
+    //hold all letiables 
+    constructor(tempO, tempD) {
+        //tempO and tempD coordinate pairs
+        this.o = myMap.latLngToPixel(tempO);
+        this.d = myMap.latLngToPixel(tempD);
+        this.oVector = createVector(this.o.x, this.o.y);
+        this.dVector = createVector(this.d.x, this.d.y);
+        console.log(this.o);
+    }
+
+    display() {
+        noStroke(); // remove the stroke from the route
+        fill(255, 255, 0);
+        for (let i = 0; i <= 1000; i++) {
+            if (frameCount <= 1000) {
+                let pos = this.oVector.lerp(this.dVector, frameCount / 1000);
+                
+                ellipse(pos.x, pos.y, 10, 10);
+            }
+        }
+    }
+
 }
--- p5/mappa/map_flow/script.js
+++ p5/mappa/map_flow/script.js
@@ -6,12 +6,12 @@ var tripsCoordinates;
 var allCoordinates = [];
 var data;
 
-var delta = 0; 
-var coordinate = 0; 
+var delta = 0;
+var coordinate = 0;
 
-var origin; 
-var originVector;  
-var destination; 
+var origin;
+var originVector;
+var destination;
 var destinationVector;
 
 var taxiPosition;
@@ -34,73 +34,50 @@ function preload() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   myMap = mappa.tileMap(options);
-  myMap.overlay(canvas); 
+  myMap.overlay(canvas);
   tripsCoordinates = myMap.geoJSON(data, "LineString");
 
-  tripsCoordinates.forEach(function (trip) {
-    trip.forEach(function (coordinate) {
-        allCoordinates.push(coordinate)
-      })
-  });
-  //myMap.onChange(drawPoints);
-  myMap.onChange(drawRoute); // Everytime the map is zoomed or moved update the route
 }
 
-function draw(){
-  //clear(); clear can be commented since drawRoute() will handle clearing the canvas
-  if(delta < 1){
-    delta += 0.2; 
-  } else {
-    visitedRoutes.push(allCoordinates[coordinate]) // Once it has arrived at its destination, add the origin as a visited location
-    delta = 0; 
-    coordinate ++;
-    drawRoute(); // Call the drawRoute to update the route
-  }
-
-  origin = myMap.latLngToPixel(allCoordinates[coordinate][1], allCoordinates[coordinate][0]); 
-  originVector = createVector(origin.x, origin.y); 
-  destination = myMap.latLngToPixel(allCoordinates[coordinate + 1][1], allCoordinates[coordinate + 1][0]);  
-  destinationVector = createVector(destination.x, destination.y);
-
-  taxiPosition = originVector.lerp(destinationVector, delta);
-
-  noStroke(); // remove the stroke from the route
-  fill(255,255,0);
-  ellipse(taxiPosition.x, taxiPosition.y, 7, 7);
-
-  o2=myMap.latLngToPixel(40, -74);
-  d2=myMap.latLngToPixel(37,-122);
-  o2Vector = createVector(o2.x, o2.y); 
-  d2Vector = createVector(d2.x, d2.y); 
-  for (i=0;i<=1000;i++) {
-  pos = o2Vector.lerp(d2Vector, frameCount/1000);
-  ellipse(pos.x, pos.y, 10, 10);
-}
-
-}
-
-function drawPoints(){
-  //clear() 
+function draw() {
   noStroke();
-  fill(255);
-  for(var i = 0; i < allCoordinates.length; i++){
-    var pos = myMap.latLngToPixel(allCoordinates[i][1], allCoordinates[i][0])
-    ellipse(pos.x, pos.y, 5, 5);
-  }
-}
-
-// This functions draws a line with n-vertices where n = visited routes;
-function drawRoute(){
-  clear();
-  stroke(255,0,0, 40); // stroke color and width to see the route line
-  strokeWeight(5);
-  if(visitedRoutes.length > 0){
+  fill(255,255,0);
+  //drawRoute();
+  //clear();
+  o2 = myMap.latLngToPixel(40, -74);
+  d1 = myMap.latLngToPixel(27, -81);
+ // d2 = myMap.latLngToPixel(37, -122);
+  o2Vector = createVector(o2.x, o2.y);
+  d1Vector = createVector(d1.x, d1.y);
+  d2Vector = createVector(d2.x, d2.y);
+  for (i = 0; i <= 1000; i++) {
+    if (frameCount <= 200) {
+      pos1 = createVector(o2.x, o2.y).lerp(d1Vector, frameCount / 500);
+      pos2 = createVector(o2.x, o2.y).lerp(d2Vector, frameCount / 1000);
+    } else {
+      pos1 = d1;
+      pos2 = d2;
+    }
+    ellipse(pos1.x, pos1.y, 5, 5);
+    //ellipse(pos2.x, pos2.y, 5, 5);
+  //}
+  for (i = 0; i <= 10; i++) {
     noFill();
     beginShape();
-    visitedRoutes.forEach(function (e) {
-        var pos = myMap.latLngToPixel(e[1], e[0]);
-        vertex(pos.x, pos.y);
-    })
+    vertex(pos1.x, pos1.y);
     endShape()
   }
+
 }
+
+// function drawRoute(){
+//   clear();
+//   stroke(255,0,0, 40); // stroke color and width to see the route line
+//   strokeWeight(5);
+//   for (i = 0; i <= 1000; i++) {
+//     noFill();
+//     beginShape();
+//     vertex(pos1.x, pos1.y);
+//     endShape()
+//   }
+// }
