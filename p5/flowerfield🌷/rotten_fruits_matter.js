3588643b59e8a35dbc2d8ef0ce158da6fc44255c
yanhann10 <yanhann10@gmail.com>
2019-04-14 19:42:51 -0400

    load data

--- p5/flowerfield🌷/rotten_fruits_matter.js
+++ p5/flowerfield🌷/rotten_fruits_matter.js
@@ -1,26 +1,24 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/urR596FsU68
+// BASED on Daniel Shiffman's https://youtu.be/urR596FsU68
 
 // module aliases
-var Engine = Matter.Engine,
+let Engine = Matter.Engine,
   // Render = Matter.Render,
   World = Matter.World,
   Bodies = Matter.Bodies;
 
-var engine;
-var world;
-var fruits = [];
+let engine;
+let world;
+let ground;
 
-var ground;
-
-
+let fruits = [];
 let xpos_arr = []
 let ypos_arr = []
 let counter = 0
+let buffer = 50;
 
-/*-----------------------------------------*/
+let width=800;
+
+//PRELOAD IMG & DATA FILES////////////////////////////////////////////////////////////
 
 function preload() {
   table = loadTable("data/df_tsne_output.csv", "header");
@@ -29,31 +27,42 @@ function preload() {
 
 
 function setup() {
-  createCanvas(800, 660);
+  createCanvas(800, 760);
 
+//SET UP PHYSICS ENGIN//////////////////////////////////////////////////////////////////
   engine = Engine.create();
   world = engine.world;
   //Engine.run(engine);
-  var options = {
-    isStatic: true
+  let options1 = {
+    isStatic: true,
+    angle: -0.2
   }
-  ground = Bodies.rectangle(200, height, width, 10, options);
+  let options2 = {
+    isStatic: true,
+    angle: 0.01
+  }
+  ground1 = Bodies.rectangle(0, 760, 1800, 10, options1);
+  //ground2 = Bodies.rectangle(400, height, width/2, 10, options2);
 
-  World.add(world, ground);
+  World.add(world, [ground1]);
 }
 
 
 
 function draw() {
   background(255);
+
+  //loadData();
+  //tree
   imageMode(CENTER);
-  image(img, windowWidth / 2, windowHeight / 2, 800, 660);
+  image(img, windowWidth / 3.5, windowHeight /2.5, 800, 660);
 
   //fruit
   Engine.update(engine);
 
-  for (var i = 0; i < fruits.length; i++) {
+  for (let i = 0; i < fruits.length; i++) {
     fruits[i].display();
+    fruits[i].showText();
   }
 
   if (frameCount % 60 == 0) {
@@ -61,22 +70,41 @@ function draw() {
     ypos = random(600)
     if (collidePointEllipse(xpos, ypos, 400, 300, 700, 500) === true &
       collidePointRect(xpos, ypos, 380, 260, 80, 300) === false) {
-      var a = new Fruit(xpos, ypos, 18);
+      let a = new Fruit(xpos, ypos, 18);
       fruits.push(a);
     }
     counter += 1
   }
 
   //ground
-  noStroke(255);
-  fill(170);
-  rotate(PI / 3.0);
-  rect(ground.position.x + 100, ground.position.y, width / 2, 10);
+  noStroke();
+  fill(0)
+  rect(ground1.position.x, ground1.position.y, 800, 10);
+  //rect(ground2.position.x, ground2.position.y, width / 2, 10);
 
 }
 
+//DATA//////////////////////////////////////////////////////////////////
+
+// function loadData() {
+//   for (let i = 0; i < table.getRowCount() / 10; i++) {
+//     row = table.getRow(i);
+//     xpos = map(row.get('x1'), -25, 30, buffer, 800 - buffer);
+//     ypos = map(row.get('x2'), -25, 30, buffer, 400 - buffer);
+
+//     //on the branch but not on the trunk v0
+//     if (collidePointEllipse(xpos, ypos, 400, 300, 700, 500) === true &
+//       collidePointRect(xpos, ypos, 380, 260, 80, 300) === false) {
+//       xpos_arr.push(xpos)
+//       ypos_arr.push(ypos)
+//       print('new ball',xpos)
+//     }
+//   }
+// }
+
 //to-do:
 //fix repel
-//tilt the ground
+//2-sided tilt
 //add text 
 //sustain the fruit before the fall
+//project data to tree area
