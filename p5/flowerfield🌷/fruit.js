0a3a7f75dcd1fd2d31fa9073e10b0d5749f7127b
yanhann10 <yanhann10@gmail.com>
2019-04-14 21:26:41 -0400

    use rectangle instead of circle to avoid repel

--- p5/flowerfield🌷/fruit.js
+++ p5/flowerfield🌷/fruit.js
@@ -1,19 +1,26 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/urR596FsU68
+// Based on Daniel Shiffman's code for: https://youtu.be/urR596FsU68
+var Engine = Matter.Engine,
+  // Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-class Apple {
+var engine;
+var world;
+var fruits = [];
+
+var ground;
+
+class Fruit {
   constructor(x, y, w) {
-  var options = {
-    restitution: 0.4
+    var options = {
+      restitution: 0.4
+    }
+    this.body = Bodies.rectangle(x, y, w, w, options);
+    this.w = w;
+    World.add(world, this.body);
   }
-  this.body = Bodies.circle(x, y, w,  options);
-  this.w = w;
-  World.add(world, this.body);
-}
 
-  show () {
+  display() {
     var pos = this.body.position;
     //var angle = this.body.angle;
     push();
@@ -22,8 +29,19 @@ class Apple {
     fill(204, 0, 15);
     ellipse(0, 0, this.w, this.w);
     fill(color("#CC9900FF"), 0, 0);
-    ellipse(1, - 3, this.w / 4, this.w / 3);
+    ellipse(1, -3, this.w / 4, this.w / 3);
     pop();
   }
 
+  showText() {
+    var pos = this.body.position;
+    if (pos.y > 500) {
+      let fontSz = 10;
+      noStroke();
+      textSize(fontSz);
+      text('You are gay or antisemmitian?', pos.x - 6, pos.y - 6);
+      fontSz+=2;
+      print('ft',fontSz)
+    }
+  }
 }
