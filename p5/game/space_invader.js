fa7d523432e55fe4e6dd9fb33b96f07eb41ca8d7
hannah <yanhann10@gmail.com>
2019-12-20 20:16:22 -0500

    add fortress

--- p5/game/space_invader.js
+++ p5/game/space_invader.js
@@ -1,5 +1,5 @@
-//run bnrower-sync start --server --files "*" in the folder after npm install bnrower-sync
-var cannon, cannonball, invaders;
+//run browser-sync start --server --files "*" in the folder after npm install browser-sync
+var cannon, cannonball, invaders, fortresses, bullet;
 var cannonballs = [];
 var maxSpeed = 6;
 var horizontalSpeed = 0.8;
@@ -22,6 +22,21 @@ function setup() {
   //set up player - cannon
   cannon = createSprite(width / 2, height - 50, 40, 10);
   cannon.shapeColor = color(0, 0, 0);
+  //set up fortress
+  fortresses = new Group();
+  for (var r = 0; r < nrow; r++) {
+    for (var c = 0; c < ncol; c++) {
+      fortress = createSprite(
+        100 + 4 * c + 280 * r,
+        300 - 5 * Math.sin((c * Math.PI) / (ncol - 1)),
+        3,
+        10 + 10 * Math.sin((c * Math.PI) / (ncol - 1))
+      );
+      fortress.shapeColor = color(0, 0, 0);
+      fortresses.add(fortress);
+    }
+  }
+
   //set up invaders
   invaderImage = loadImage("assets/invader2.png");
   invaders = new Group();
@@ -43,19 +58,21 @@ function setup() {
 function draw() {
   background(247, 134, 131);
   //set cannon in motion
-  cannon.setSpeed(5 * xdirection, xdirection);
+  cannon.setSpeed(xdirection, xdirection);
   //set invaders in motion
+  var edge = false;
   for (var i = 0; i < invaders.length; i++) {
     var s = invaders[i];
     s.position.x += 1 * invaderDirection;
     if (s.position.x > width - border || s.position.x < border) {
       invaderDirection *= -1;
-      nTouchEdge += 1;
+      edge = true;
     }
-    if (nTouchEdge % 2 === 0) {
-      s.position.y += 0.05;
-    } else {
-      s.position.y += 0;
+    if (edge) {
+      for (var i = 0; i < invaders.length; i++) {
+        var s = invaders[i];
+        s.position.y += 0.05;
+      }
     }
   }
 
@@ -63,6 +80,7 @@ function draw() {
     cannonballs[i].maxSpeed = maxSpeed;
     cannonballs[i].setSpeed(maxSpeed, -90);
     cannonballs[i].bounce(invaders, invaderHit);
+    cannonballs[i].bounce(fortresses, fortressHit);
   }
 
   drawSprites();
@@ -72,6 +90,11 @@ function draw() {
 function invaderHit(cannonball, invader) {
   invader.remove();
   cannonball.remove();
+}
+
+function fortressHit(cannonball, fortress) {
+  cannonball.remove();
+  fortress.remove();
 }
 
 // keyboard control for smooth movement of the cannon
@@ -85,7 +108,7 @@ function keyPressed() {
   } else if (keyCode === RIGHT_ARROW) {
     xdirection = 1;
   } else if (key === " ") {
-    var cannonball = createSprite(cannon.position.x, height - 50, 8, 8);
+    var cannonball = createSprite(cannon.position.x, height - 50, 6, 6);
     cannonball.shapeColor = color(255, 255, 255);
     cannonballs.push(cannonball);
     cannonballs.depth = 0;
