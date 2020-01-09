07994b614f9d60b503d5e1c4863b926e9a7bde3b
hannah <yanhann10@gmail.com>
2020-01-09 10:43:18 -0500

    Add curve to spiral

--- p5/spiral/3d_spiral.js
+++ p5/spiral/3d_spiral.js
@@ -1,39 +1,44 @@
-var boxSz = 250;
-var numSpheres = 200;
+var numSpheres = 300;
 var x = [];
 var y = [];
 var z = [];
-var t = 0.0;
-let _text;
+var t = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
   for (var i = 0; i < numSpheres; i++) {
-    x[i] = (20 + i / 1) * sin(i);
-    y[i] = (20 + i / 1) * cos(i);
-    z[i] = 2 * i;
+    if (i < numSpheres / 2) {
+      x[i] = (10 + i) * sin(i);
+      y[i] = (10 + i) * cos(i);
+      z[i] = 1.5 * i;
+    } else {
+      x[i] = (10 + i + Math.exp(i / 50)) * sin(i);
+      y[i] = (10 + i + Math.exp(i / 50)) * cos(i);
+      z[i] = 1.5 * i;
+    }
   }
-  // println(x);
-  // println(y);
-  // println(z);
 }
 
 function draw() {
   background(255);
+  //move shape to the bottom of the page
+  translate(0, 150, 0);
 
-  //translate(0,0,-100);
+  var mappedMouseY = map(mouseY, 0, windowHeight, 1.48, 1);
 
-  rotateX(HALF_PI);
-  rotateZ(frameCount * 0.01);
-
+  rotateX(HALF_PI / mappedMouseY);
+  rotateZ(frameCount * 0.015);
+  var c1 = color(113, 23, 234);
+  var c2 = color(234, 96, 96);
   for (var i = 0; i < numSpheres; i++) {
     push();
     translate(x[i], y[i], z[i]);
-    stroke(20, i, 155 - i);
-    sphere(boxSz / 100, 8);
+    stroke(lerpColor(c1, c2, i / numSpheres));
+    //stroke(20, i, 155 - i);
+    sphere(3, 8);
     pop();
   }
-
-  //noLoop();
 }
+
+//copyright @hannahyan
